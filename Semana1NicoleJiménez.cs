d6cf9e5aa3dfe292153f0c08e9ee09c41299fb00
nicjhub <nico.tatianajim@gmail.com>
2026-02-18 21:18:10 -0600

    Add files via upload

---
+++ Semana1NicoleJiménez.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            Console.Write("Ingresa tu nombre: "); 
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingresa tu edad: "); 
+            int edad = int.Parse(Console.ReadLine());
+
+            // Da la info
+            Console.WriteLine($"Hola {nombre}, tienes {edad} años.\"); 
+
+            int edadFutura = edad + 5;
+            Console.WriteLine($"En 5 años tendrás {edadFutura} años.\");
+            // Valida las edades
+            if (edad >= 18)
+                {
+                Console.WriteLine("Eres mayor de edad.\"); 
+                }
+            else
+                {
+                Console.WriteLine("Eres menor de edad.\");
+                }
+
+        }
+        
+    }
+}
